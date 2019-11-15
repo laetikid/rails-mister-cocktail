397718371f0850aa50056e63b6ce1e41419e6047
laetitia danton <danton.laetitia@gmail.com>
2019-11-15 11:06:53 +0100

    mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,9 +16,9 @@ class DosesController < ApplicationController
 end
 
   def destroy
-    @dose = Dose.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    #redirect_to cocktails_path()
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
 private
